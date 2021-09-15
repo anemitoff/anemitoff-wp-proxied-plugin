4adbd5744609cbbdfe634e03942891bf18829c13
Adam Nemitoff <adamn@teamnemitoff.com>
2021-09-15 14:37:29 -0400

    -- Fixes -- Exclude jetpack from redirects -- Fix broken wp-admin redirect exlusion -- Fix case where $_SERVER[REQUEST_SCHEME] not valid on wordpress.com server

--- anemitoff-wp-proxied.php
+++ anemitoff-wp-proxied.php
@@ -2,7 +2,7 @@
 /*
  * Plugin name: Anemitoff WP Proxied
  * Description: When blog is accessed by proxy, links are rewritten to appear as though they belog to proxy origin
- * Version: 1.1.3
+ * Version: 1.1.6
  * Author: Adam Nemitoff
  * Author URI: https://teamnemitoff.com
  * License: GPL v3
@@ -11,11 +11,10 @@
 
 if (!class_exists(AnemitoffWpProxiedPlugin::class)) {
     class AnemitoffWpProxiedPlugin {
-        const USE_CANONICAL_HOST = false;
-        const CANONICAL_HOST = 'nassaucandy.com'; // this must reverse proxy to actual blog host
+        const USE_CANONICAL_HOST = true;
+        const CANONICAL_HOST = 'www.nassaucandy.com';
+        //const CANONICAL_HOST = 'ncweb241.teamnemitoff.com'; // this must reverse proxy to actual blog host
         const USE_TEMPORARY_REDIRECT_FOR_CANONICAL_HOST = true;
-
-
 
         function __construct() {
             add_filter('post_link', [$this, 'filterPostLink']);
@@ -23,41 +22,46 @@ if (!class_exists(AnemitoffWpProxiedPlugin::class)) {
             add_filter('wp_page_menu_args', [$this,  'filterPageMenuArgs']); // NOOP
             add_filter('home_url', [$this, 'filterHomeUrl']);
             add_filter('the_content', [$this, 'filterTheContent']);
-			add_filter('get_custom_logo', [$this, 'getCustomLogo']);
+            add_filter('get_custom_logo', [$this, 'getCustomLogo']);
 
             add_action('init', [$this, 'useCanonicalHostIfForwarded']);
         }
 
-		function getCustomLogo($html) {
+        function getCustomLogo($html) {
             $forwardingHost = $_SERVER['HTTP_X_FORWARDED_HOST'] ?? null; 
             if (!$forwardingHost) return $html;
             $html = preg_replace('/\/blog/','',$html, 1);
             return $html;
         }
-		
+
         function useCanonicalHostIfForwarded() {
             if (!SELF::USE_CANONICAL_HOST) return;
-			
+
             $forwardingHost = $_SERVER['HTTP_X_FORWARDED_HOST'] ?? ''; 
             if (SELF::CANONICAL_HOST == $forwardingHost) {
                 return;
             }
 
             $originalUrl = SELF::original_url();
-			if (mb_strpos($originalUrl, 'wp-admin') !== false) {
+            if (strpos($originalUrl, 'wp-admin') !== false) {
 				return;
 			}
-			
-			
+
+            if (strpos($originalUrl, 'jetpack') !== false) {
+				return;
+			}
+
             $_SERVER['HTTP_X_FORWARDED_HOST'] = SELF::CANONICAL_HOST;
             $canonicalUrl = SELF::rewriteUrlForForwarding($originalUrl);
+			
             wp_redirect($canonicalUrl, SELF::USE_TEMPORARY_REDIRECT_FOR_CANONICAL_HOST ? 302 : 301);
             die();
         }
 
 
         private static function original_url() {
-            return "$_SERVER[REQUEST_SCHEME]://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+			$requestScheme = $_SERVER['REQUEST_SCHEME'] ?? 'https';
+            return "$requestScheme://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
         }
 
         private static function build_url(array $elements) {
